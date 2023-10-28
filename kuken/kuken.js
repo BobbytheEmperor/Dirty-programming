925992b28a2b5ead4488ebb9830e4f8d4cca2565
Pauline <pasp2300@student.miun.se>
2023-10-28 14:33:48 +0200

    Kuk i html format.

--- kuken/kuken.js
+++ kuken/kuken.js
@@ -1,23 +1,59 @@
 
 "Use Strict"
 
-function growPenis(){
+function growPenis() {
 
-let penis = document.getElementById('penis');
+    let penis = document.getElementById('penis');
 
-let penisText = penis.innerText; 
+    let penisText = penis.innerText;
 
-let penisLenght = penisText.length - 2;
+    let penisLength = penisText.length - 2;
 
-let newPenis = "8" + "=" + "D";
+    let newPenisLength = penisLength + 1;
 
-let shaft = "=";
+    let newPenis = "8" + "=" + "D";
 
-while(shaft.length)
+    let shaft = "=";
 
+    while (shaft.length < newPenisLength) {
+
+        shaft += "=";
+
+    }
+
+    newPenis = "8" + shaft + "D";
+
+    penis.innerHTML = newPenis;
+
+}
+
+function shrinkPenis() {
+
+    let penis = document.getElementById('penis');
+
+    let penisText = penis.innerText;
+
+    let penisLenght = penisText.length - 2;
+
+    let newPenisLength = penisLenght - 1;
+
+    let newPenis = "8" + "=" + "D";
+
+    let shaft = "=";
+
+    while (shaft.length < newPenisLength) {
+
+        shaft += "="; 
+
+    }
+
+    newPenis = "8" + shaft + "D";
+
+    penis.innerHTML = newPenis;
 
 
 }
+
 
 let growPenisButton = document.getElementById("growPenis");
 
@@ -25,4 +61,4 @@ growPenisButton.addEventListener('click', growPenis);
 
 let shrinkPenisButton = document.getElementById("shrinkPenis");
 
-
+shrinkPenisButton.addEventListener('click', shrinkPenis);
