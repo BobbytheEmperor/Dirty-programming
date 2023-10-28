96b4f330a3516471adff44a1e4b871bd97762ec4
Pauline <pasp2300@student.miun.se>
2023-10-28 13:26:51 +0200

    Dirty programming uppgift 1.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Dirty-programming
 
 Här kommer det vara lite kul programmering. 
+
+## Kuken
+
+Krav: 
+Kuken ska växa genom knapptryck
+Kuken ska minska genom knapptryck på annan knapp
+Kuken får inte bli kortare än 3 tecken (8=D)
+Kuken får inte bli längre än 20 tecken
+Kuken ska visas på en html-sida
---
+++ kuken/kuken.js
@@ -0,0 +1,28 @@
+
+"Use Strict"
+
+function growPenis(){
+
+let penis = document.getElementById('penis');
+
+let penisText = penis.innerText; 
+
+let penisLenght = penisText.length - 2;
+
+let newPenis = "8" + "=" + "D";
+
+let shaft = "=";
+
+while(shaft.length)
+
+
+
+}
+
+let growPenisButton = document.getElementById("growPenis");
+
+growPenisButton.addEventListener('click', growPenis);
+
+let shrinkPenisButton = document.getElementById("shrinkPenis");
+
+
